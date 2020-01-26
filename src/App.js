abda7f0666b1fb2110d8ae01d7ee899bf5fcfa36
devolper7@gmail.com <fortuna2998>
2020-01-26 03:21:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import HomePage from "./modules/homepage/HomePage";
+
+class App extends Component {
+  render() {
+    return <div>
+      <HomePage/>
+    </div>;
+  }
+}
+
+export default App;

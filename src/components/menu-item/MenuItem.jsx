7e1d699635ed4aba15b0c5d05426b3daab783251
devolper7@gmail.com <fortuna2998>
2020-02-17 13:36:18 +0200

    added new font, added animation

--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 import './menuitem.scss';
 
-const MenuItem = ({ title, imageUrl, id }) => (
+const MenuItem = ({ title, imageUrl, size }) => (
   <div
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className="menu-item">
+    className={`${size} menu-item`}>
+    <div className="background-image"
+         style={{
+           backgroundImage: `url(${imageUrl})`
+         }}>
+
+    </div>
     <div className="content">
-      <h1 className="title">{ title }</h1>
+      <h1 className="title">{ title.toUpperCase( ) }</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
